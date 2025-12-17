3453f251e2d5267b46513f9f763058f6644e62fe
JTeixeiraz <joaopedroteixeirareis@gmail.com>
2025-12-17 00:01:56 -0300

    ajustando o segundo andar

--- lib/screens/present/floor2Scenes/thirdSceneFloor2.dart
+++ lib/screens/present/floor2Scenes/thirdSceneFloor2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:torredorelogio/Widgets/Icons/IconeAction.dart';
 import 'package:torredorelogio/Widgets/Icons/LeftIcon.dart';
 import 'package:torredorelogio/Widgets/Icons/RigthIcon.dart';
 
@@ -10,6 +11,24 @@ class Thirdscenefloor2 extends StatefulWidget {
 }
 
 class _Thirdscenefloor2State extends State<Thirdscenefloor2> {
+
+  bool cadeadoAberto = false;
+  final TextEditingController senha = new TextEditingController();
+
+
+  void abrirCadeado(){
+    setState(() {
+      cadeadoAberto = true;
+    });
+  }
+
+  void fecharCadeado(){
+    setState(() {
+      cadeadoAberto = false;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,14 +37,88 @@ class _Thirdscenefloor2State extends State<Thirdscenefloor2> {
         children: [
           Positioned.fill(
           child: Image.asset(
-            'assets/Cena3SegundoAndar.png',
+            'assets/visaoAltarRefeita.png',
             fit: BoxFit.cover,
           ),
         ),
 
+        IconeWidget(
+          height: 0.55, 
+          width: 0.70, 
+          icone: Icons.lock, 
+          onTap: abrirCadeado
+        ),
 
-
-
+        if(cadeadoAberto)
+        Positioned.fill(
+          child: GestureDetector(
+            onTap: fecharCadeado,
+            child: Container(
+              color: Colors.black.withOpacity(0.7),
+              child: Center(
+                child: GestureDetector(
+                  onTap: () {
+                    
+                  },
+                  child: SingleChildScrollView(
+                    child: Container(
+                      margin: const EdgeInsets.all(20),
+                      constraints: const BoxConstraints(maxWidth: 300),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(20),
+                            child: Column(
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.all(16),
+                                  decoration: BoxDecoration(
+                                        color: Colors.white.withOpacity(0.9),
+                                        borderRadius: BorderRadius.circular(8),
+                                        border: Border.all(
+                                          color: const Color(0xFF92400E).withOpacity(0.2),
+                                          width: 4,
+                                        ),
+                                      ),
+                                      child: Column(
+                                        children: [
+                                          Icon(Icons.lock, size: 50, color: Colors.black,),
+                                          TextField(
+                                            controller: senha,
+                                            decoration: InputDecoration(
+                                              hint: Text('Senha...', style: TextStyle(color: Colors.black),),
+                                            ),
+                                            style: TextStyle(
+                                              color: Colors.black
+                                            ),
+                                          ),
+                                          const SizedBox(height: 20,),
+                                          ElevatedButton(
+                                            style: ElevatedButton.styleFrom(
+                                              foregroundColor: Colors.white,
+                                              padding: const EdgeInsets.all(10)
+                                            ),
+                                            onPressed: (){
+                                              
+                                            },
+                                            child: Text("Abrir"),
+                                          ),
+                                        ],
+                                      )
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
 
           Lefticon(route: '/segundoAndarPresente'),
           Rigthicon(route: '/segundoAndarTras')
